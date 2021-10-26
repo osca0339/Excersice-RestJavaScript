c986ce8ef1b9e20012fbf7831a2922ea5dc3b862
Oscar Arklint Hemmingsen <osca5515@edu.easj.dk>
2021-10-26 10:04:33 +0200

    Made Model and manager class

---
+++ MusicRest/MusicRest/Models/Music.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicRest.Models
+{
+    public class Music
+    {
+        private int _publicationYear;
+
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Duration { get; set; }
+        public int PublicationYear
+        {
+            get => _publicationYear;
+            set
+            {
+                if(value > 2021)
+                {
+                    throw new ArgumentException();
+                }
+                _publicationYear = value;
+            }
+        }
+
+        public Music(string title, string artist, int duration, int publicationYear)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            PublicationYear = publicationYear;
+        }
+    }
+}
---
+++ MusicRest/MusicRest/MusicManager.cs
@@ -0,0 +1,19 @@
+﻿using MusicRest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicRest
+{
+    public class MusicManager
+    {
+        private static List<Music> musicList = new List<Music>()
+        {
+            new Music("My Heart Goes On", "Celine Dion", 100, 1999),
+            new Music("Bad", "Michael Jackson", 120, 1988),
+            new Music("The Best", "Foo Fighters", 110, 2002)
+        };
+    }
+}
+
