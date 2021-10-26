a4a8e3931b1bcea650a1a63c0fbb276497c250c2
Jonathan Busk <jona881d@edu.easj.dk>
2021-10-26 10:27:50 +0200

    Lavet på Get, Create og update

--- MusicRest/MusicRest/MusicManager.cs
+++ MusicRest/MusicRest/MusicManager.cs
@@ -14,6 +14,27 @@ namespace MusicRest
             new Music("Bad", "Michael Jackson", 120, 1988),
             new Music("The Best", "Foo Fighters", 110, 2002)
         };
+
+        public List<Music> Get()
+        {
+            return musicList;
+        }
+
+        public Music Get(string title)
+        {
+            return musicList.Find(f => f.Title == title);
+        }
+
+        public bool Create(Music Value)
+        {
+            musicList.Add(Value);
+            return true;
+        }
+
+        public bool Update(int id, Music Value)
+        {
+            Music music = Get(id)
+        }
     }
 }
 
